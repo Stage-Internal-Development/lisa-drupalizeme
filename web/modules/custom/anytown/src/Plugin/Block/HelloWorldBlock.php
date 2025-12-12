b04f5da0b6238086cb3247271f1bfa6d06f2b1fe
Lisa Corcoran <lisa.corcoran@stage-entertainment.com>
2025-12-12 14:14:47 +0000

    chore: update block plugin to use dependency injection to access the current user service and print the user name

--- web/modules/custom/anytown/src/Plugin/Block/HelloWorldBlock.php
+++ web/modules/custom/anytown/src/Plugin/Block/HelloWorldBlock.php
@@ -7,6 +7,9 @@ namespace Drupal\anytown\Plugin\Block;
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Hello World' block.
@@ -17,15 +20,43 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
   admin_label: new TranslatableMarkup('Hello World'),
   category: new TranslatableMarkup('Custom'),
 )]
-class HelloWorldBlock extends BlockBase {
+class HelloWorldBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  private $currentUser;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build(): array {
-    $build['content'] = [
-      '#markup' => $this->t('Hello World!'),
+    $name = $this->currentUser->getDisplayName();
+    if ($this->currentUser->isAuthenticated()) {
+      $build['content'] = [
+        '#markup' => $this->t('Hello, %name', ['%name' => $name]),
+      ];
+    } else {
+      $build['content'] = [
+        '#markup' => $this->t('Hello, Guest'),
+      ];
+    }
+
+    $build['content']['#cache'] = [
+      'max-age' => 0,
     ];
+    
     return $build;
   }
 }
