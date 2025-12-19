8c914d1e2fee224d580e0e952b020d4744a77c3d
Lisa Corcoran <lisa.corcoran@stage-entertainment.com>
2025-12-19 15:22:55 +0000

    chore: admin settings for extending base form with route and menu link

---
+++ web/modules/custom/anytown/src/Form/SettingsForm.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\anytown\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Anytown settings for this site.
+ */
+final class SettingsForm extends ConfigFormBase {
+
+  /**
+   * Name for module's configuration object.
+   */
+  const SETTINGS = 'anytown.settings';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return self::SETTINGS;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return [self::SETTINGS];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['display_forecast'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display weather forecast'),
+      '#default_value' => $this->config(self::SETTINGS)->get('display_forecast'),
+    ];
+
+    $form['location'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('ZIP code of market'),
+      '#description' => $this->t('Used to determine weekend weather forecast.'),
+      '#default_value' => $this->config(self::SETTINGS)->get('location'),
+      '#placeholder' => '90210',
+    ];
+
+    $form['weather_closures'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Weather-related closures'),
+      '#description' => $this->t('List one closure per line.'),
+      '#default_value' => $this->config(self::SETTINGS)->get('weather_closures'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+}
